d81d6e34268d3901abe1abfa6e0e588f761c5652
Kjeff24 <jeffreyarthur123@gmail.com>
2023-05-09 18:07:29 +0000

    Successfully created login, signup for employees and employers

--- myapp/forms.py
+++ myapp/forms.py
@@ -2,9 +2,10 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from .models import User
 
+# login form
 class LoginForm(forms.Form):
     username = forms.CharField(
-        widget= forms.TextInput(
+        widget=forms.TextInput(
             attrs={
                 "class": "form-control"
             }
@@ -18,8 +19,13 @@ class LoginForm(forms.Form):
         )
     )
 
+employer_choices = [(user.username, user.username) for user in User.objects.filter(is_employer=True)]
 
-class SignUpForm(UserCreationForm):
+# Employee signup
+class EmployeeSignUpForm(UserCreationForm):
+    
+    employer = forms.ChoiceField(choices=employer_choices, widget=forms.Select(attrs={'class': 'my-employer-choice'}))
+
     username = forms.CharField(
         widget=forms.TextInput(
             attrs={
@@ -51,4 +57,51 @@ class SignUpForm(UserCreationForm):
 
     class Meta:
         model = User
-        fields = ('username', 'email', 'password1', 'password2', 'is_employer', 'is_employee')
+        fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2',
+                  'is_employee', 'employer')
+    
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.my_employer = self.cleaned_data['employer']
+        if commit:
+            user.save()
+        return user
+        
+
+    
+# Employer Signup
+class EmployerSignUpForm(UserCreationForm):
+    username = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                "class": "form-control"
+            }
+        )
+    )
+    password1 = forms.CharField(
+        widget=forms.PasswordInput(
+            attrs={
+                "class": "form-control"
+            }
+        )
+    )
+    password2 = forms.CharField(
+        widget=forms.PasswordInput(
+            attrs={
+                "class": "form-control"
+            }
+        )
+    )
+    email = forms.CharField(
+        widget=forms.EmailInput(
+            attrs={
+                "class": "form-control"
+            }
+        )
+    )
+
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'last_name', 'email', 'password1',
+                  'password2', 'is_employer')
--- myapp/models.py
+++ myapp/models.py
@@ -4,5 +4,6 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
 class User(AbstractUser):
-    is_employer = models.BooleanField("Is employer", default=False)
-    is_employee = models.BooleanField("Is employee", default=False)
+    is_employer = models.BooleanField("Is employer", default=True)
+    is_employee = models.BooleanField("Is employee", default=True)
+    my_employer = models.CharField(max_length=200, null=True, blank=True)
